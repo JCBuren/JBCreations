667ee998a7b352c4bdc58999604e8c2228cfd458
Jordan <JBuren1957@Gmail.com>
2022-12-14 05:25:04 -0600

    styling

--- components/Main.jsx
+++ components/Main.jsx
@@ -6,7 +6,7 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 const Main = () => {
     return (
         <div id='home' className='w-full h-screen text-center '>
-            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex items-center'>
+            <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
                     <h1 className='py-4'>
                         Hi, I&#39;m <span className='text-[#B57B49]'> Jordan </span>
@@ -14,7 +14,7 @@ const Main = () => {
                     <h1 className='py-4'>
                         A Full-Stack Web Developer
                     </h1>
-                    <p className='font-bold py-8 max-w-[70%] m-auto'>
+                    <p className='py-4 sm:max-w-[70%] m-auto'>
                     I&#39;m a full stack web developer with experience in JavaScript, React, Ruby on Rails, CSS, as well as Next.JS and Tailwind. My passion has always been helping people, and learning the many ways you can leverage responsive front-end technologies has given me a fresh perspective on ways I can do just that.
                     </p>
                     <div className=' flex justify-between max-w-[580px] m-auto py-8'>
