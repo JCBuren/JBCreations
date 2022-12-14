1cf85861ac20f616481d86d768c696153f891daf
Jordan <JBuren1957@Gmail.com>
2022-12-14 05:40:10 -0600

    styling

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 const Main = () => {
     return (
-        <div id='home' className='w-full h-screen text-center tracking-widest'>
+        <div id='home' className='w-full h-screen text-center justify-center '>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
                     <h1 className='py-4'>
